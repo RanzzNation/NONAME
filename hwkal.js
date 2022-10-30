55488c133a9b3e4a9cf2b5c35436f0d9985830b5
RanzzNation <116924759+RanzzNation@users.noreply.github.com>
2022-10-30 17:09:14 +0700

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -13,25 +13,25 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //=================================================//
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UC7flTES4TqWBLOPxk25_tsA'
-global.ig = 'https://www.instagram.com/p/CBqPkqsH344/?igshid=YmMyMTA2M2Y='
-global.mygc = 'wa.me/6281227607864'
-global.myweb = 'https://eclass.iainsalatiga.ac.id/app/upload/users/1/10892/my_files/XhiroMhonshine.html'
-global.linkgrupss = "https://chat.whatsapp.com/Fj5z7EXvGLVARaZ0cJF8rd"
-global.linkpro = 'mailto:support@whatsapp.com?subject=Deactivate%20My%20Account%20Number&body=Please%20Deactivate%20The%20My%20Account%20Number%20%2B62XXXXXXXX%20Immediately%20Because%20The%20Number%20Has%20Been%20Lost'
-global.email = 'hendralubeck86@gmail.com'
+global.youtube = 'https://youtube.com/c/ahmadstoreeee'
+global.ig = 'https://instagram.com/ahmadstoreeee?igshid=MDE2OWE1N2Q='
+global.mygc = 'wa.me/6289697155781'
+global.myweb = '-'
+global.linkgrupss = "-"
+global.linkpro = '-'
+global.email = 'storeahmad362@gmail.com'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'HENDRA LUBECK' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'NONAME' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'HENDRA' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Ranzz Nation' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Ranzz Botz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Ranzz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6281227607864']
-global.premium = ['6281227607864']
-global.packname = 'HENDRA LUBECK'
-global.ownerr = ['NONAME']
-global.author = 'HENDRA LUBECK'
+global.owner = ['6289697155781']
+global.premium = ['6289697155781']
+global.packname = 'Ranzz Nation'
+global.ownerr = ['Ranzz']
+global.author = 'Ranzz'
 global.sessionName = './sessionye/haikal'
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sp = '⭔'
